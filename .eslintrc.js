6695ae1dad68a0cb07697d75ca050320b806aa20
aniiantt <nsso@foxmail.com>
2020-04-15 09:38:10 +1200

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
     '@typescript-eslint/require-await': 'off', // Async clearly indicates that it is an asynchronous function
     '@typescript-eslint/prefer-regexp-exec': 'off', // There is no need to force the use of this rule
     '@typescript-eslint/no-explicit-any': 'off', // Any is necessary
-    '@typescript-eslint/explicit-function-return-type': 'off' // Type inference
+    '@typescript-eslint/explicit-function-return-type': 'off', // Type inference
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/camelcase': 'off'
   }
 };
