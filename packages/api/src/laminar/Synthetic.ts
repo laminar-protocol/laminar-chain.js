552050ac5ed9c9456821f218ae4ce6d5bdf5ab39
aniiantt <nsso@foxmail.com>
2020-04-20 09:23:48 +1200

    new laminar api

--- packages/api/src/laminar/Synthetic.ts
+++ packages/api/src/laminar/Synthetic.ts
@@ -1,10 +1,10 @@
-import { Balance, Permill, SyntheticLiquidityPoolOption } from '@laminar/types/interfaces';
+import { Balance, Permill, SyntheticLiquidityPoolOption, PositionId } from '@laminar/types/interfaces';
 import { Option } from '@polkadot/types/codec';
 import BN from 'bn.js';
-import { combineLatest } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { TokenId } from '../types';
+import { TokenId, SyntheticPoolInfo } from '../types';
 import LaminarApi from './LaminarApi';
 
 class Synthetic {
@@ -26,7 +26,7 @@ class Synthetic {
     return this.apiProvider.extrinsicHelper(extrinsic, account, { action: 'Withdraw Liquidity' });
   }
 
-  poolInfo(poolId: string) {
+  poolInfo(poolId: string): Observable<SyntheticPoolInfo | null> {
     return combineLatest([
       this.api.query.baseLiquidityPoolsForSynthetic.owners(poolId),
       this.api.query.baseLiquidityPoolsForSynthetic.balances<Option<Balance>>(poolId),
@@ -39,39 +39,45 @@ class Synthetic {
           poolId: poolId,
           owners: owners.isEmpty ? null : (owners as any).value[0].toJSON(),
           balance: balances.toString(),
-          options: liquidityPoolOptions
-            .map(([storageKey, options]) => {
-              const tokenId = storageKey.args[1].toJSON();
+          options: liquidityPoolOptions.map(([storageKey, options]) => {
+            const tokenId = storageKey.args[1].toJSON();
 
-              const data = options.toHuman() || {};
+            const data = options.toHuman() || {};
 
-              const additionalCollateralRatio =
-                minAdditionalCollateralRatio.toHuman() > (data as any).additionalCollateralRatio
-                  ? minAdditionalCollateralRatio.toHuman()
-                  : (data as any).additionalCollateralRatio;
+            const additionalCollateralRatio =
+              minAdditionalCollateralRatio.toHuman() > (data as any).additionalCollateralRatio
+                ? minAdditionalCollateralRatio.toHuman()
+                : (data as any).additionalCollateralRatio;
 
-              return {
-                tokenId: tokenId,
-                ...(data as any),
-                additionalCollateralRatio
-              };
-            })
-            .reduce((result, curr) => {
-              result[curr.tokenId] = curr;
-              return result;
-            }, {} as Record<string, any>)
+            return {
+              tokenId: tokenId,
+              ...(data as any),
+              additionalCollateralRatio
+            };
+          })
         };
       })
     );
   }
 
+  public allPoolIds = () => {
+    return this.api.query.baseLiquidityPoolsForSynthetic
+      .nextPoolId<PositionId>()
+      .pipe(map(result => [...new Array(result.toNumber())].map((_, i) => `${i}`)));
+  };
+
   public redeem = async (account: string, poolId: string, fromToken: TokenId, fromAmount: string | BN) => {
-    const extrinsic = this.api.tx.syntheticProtocol.redeem(poolId, fromToken as any, fromAmount, '1000000');
+    const extrinsic = this.api.tx.syntheticProtocol.redeem(poolId, fromToken as any, fromAmount, '0');
     return this.apiProvider.extrinsicHelper(extrinsic, account, { action: 'Swap' });
   };
 
   public mint = async (account: string, poolId: string, toToken: TokenId, fromAmount: string | BN) => {
-    const extrinsic = this.api.tx.syntheticProtocol.mint(poolId, toToken as any, fromAmount, '1000000');
+    const extrinsic = this.api.tx.syntheticProtocol.mint(
+      poolId,
+      toToken as any,
+      fromAmount,
+      '100000000000000000000000'
+    );
     return this.apiProvider.extrinsicHelper(extrinsic, account, { action: 'Swap' });
   };
 }
