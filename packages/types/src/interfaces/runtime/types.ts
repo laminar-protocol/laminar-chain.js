70ddef7df6af7861f41dc928824fdcf2c8e5e13f
Bryan Chen <xlchen1291@gmail.com>
2020-05-22 16:05:30 +1200

    update currency id

--- packages/types/src/interfaces/runtime/types.ts
+++ packages/types/src/interfaces/runtime/types.ts
@@ -3,7 +3,14 @@
 
 import { ITuple } from '@polkadot/types/types';
 import { Compact, Enum, Int, Option, Set, Struct, U8aFixed, Vec } from '@polkadot/types/codec';
-import { GenericAccountId, GenericAccountIndex, GenericAddress, GenericBlock, GenericCall, GenericConsensusEngineId } from '@polkadot/types/generic';
+import {
+  GenericAccountId,
+  GenericAccountIndex,
+  GenericAddress,
+  GenericBlock,
+  GenericCall,
+  GenericConsensusEngineId
+} from '@polkadot/types/generic';
 import { Bytes, DoNotConstruct, Null, StorageKey, bool, i128, u128, u32, u64, u8 } from '@polkadot/types/primitive';
 import { Price } from '@open-web3/orml-types/interfaces/prices';
 import { AuthorityId } from '@polkadot/types/interfaces/consensus';
@@ -71,6 +78,11 @@ export interface CurrencyId extends Enum {
   readonly isFjpy: boolean;
   readonly isFbtc: boolean;
   readonly isFeth: boolean;
+  readonly isFaud: boolean;
+  readonly isFcad: boolean;
+  readonly isFchf: boolean;
+  readonly isFxau: boolean;
+  readonly isFoil: boolean;
 }
 
 /** @name CurrencyIdOf */
