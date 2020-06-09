6e88738a69d97fe5f5e3be4b63ede338936d783a
aniiantt <nsso@foxmail.com>
2020-06-10 11:27:25 +1200

    fix types

--- packages/types/src/augment-api.ts
+++
@@ -1,7 +0,0 @@
-// Auto-generated via `yarn polkadot-types-from-defs`, do not edit
-/* eslint-disable @typescript-eslint/no-empty-interface */
-
-import '@polkadot/api/augment/rpc';
-import './augment-api-consts';
-import './augment-api-tx';
-import './augment-api-query';
