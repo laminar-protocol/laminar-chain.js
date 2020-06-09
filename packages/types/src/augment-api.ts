d97c2b6e3c01ba8149b4fb3640d2e4865ea43a10
aniiantt <nsso@foxmail.com>
2020-06-10 11:30:51 +1200

    add augment-api

---
+++ packages/types/src/augment-api.ts
@@ -0,0 +1,7 @@
+// Auto-generated via `yarn polkadot-types-from-defs`, do not edit
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+import '@polkadot/api/augment/rpc';
+import './augment-api-consts';
+import './augment-api-tx';
+import './augment-api-query';
