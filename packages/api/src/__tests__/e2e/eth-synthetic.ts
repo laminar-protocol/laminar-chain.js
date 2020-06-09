9816ba81da5bf9e995fa6fe2b0aa31db6ce0ce22
nsso <nxiiun@gmail.com>
2020-06-10 10:14:52 +1200

    add SyntheticFlowTokenContract (#125)

--- packages/api/src/__tests__/e2e/eth-synthetic.ts
+++ packages/api/src/__tests__/e2e/eth-synthetic.ts
@@ -5,7 +5,7 @@ describe('ethereum api', () => {
   jest.setTimeout(300000);
 
   const api = new EthereumApi({
-    provider: new Web3.providers.HttpProvider('https://kovan.infura.io/v3/16a5aa3a08c24d56b1586cd06b4055d5')
+    provider: new Web3.providers.WebsocketProvider('wss://kovan.infura.io/ws/v3/16a5aa3a08c24d56b1586cd06b4055d5')
   });
 
   beforeAll(async () => {
@@ -22,5 +22,11 @@ describe('ethereum api', () => {
     api.synthetic.poolInfo('0x61a7645ef693ea7740b121232ddc7b874be7fa09').subscribe(result => {
       console.log(result);
     });
+  });
+
+  it('getSyntheticFlowTokenContract', async done => {
+    const a = api.getSyntheticFlowTokenContract('0x779DB2A890B7EbfFb9B38631Deb81C1febfB5361');
+    const r = await a.methods.balanceOf('0x885501bcfBad1cAE12B4FD2272F1AbdE6dd88B38').call();
+    console.log(r);
   });
 });
