4fccc9443897177ed8f8c8f86fda8b986eaacace
nsso <nxiiun@gmail.com>
2020-06-10 11:34:05 +1200

    fix types (#127)
    * fix types
    
    * add augment-api

--- packages/types/src/interfaces/runtime/types.ts
+++ packages/types/src/interfaces/runtime/types.ts
@@ -3,7 +3,14 @@
 
 import { ITuple } from '@polkadot/types/types';
 import { Compact, Enum, Int, Option, Set, Struct, U8aFixed, Vec } from '@polkadot/types/codec';
-import { GenericAccountId, GenericAccountIndex, GenericAddress, GenericBlock, GenericCall, GenericConsensusEngineId } from '@polkadot/types/generic';
+import {
+  GenericAccountId,
+  GenericAccountIndex,
+  GenericAddress,
+  GenericBlock,
+  GenericCall,
+  GenericConsensusEngineId
+} from '@polkadot/types/generic';
 import { Bytes, DoNotConstruct, Null, StorageKey, bool, i128, u128, u32, u64, u8 } from '@polkadot/types/primitive';
 import { Price } from '@open-web3/orml-types/interfaces/prices';
 import { AuthorityId } from '@polkadot/types/interfaces/consensus';
@@ -296,6 +303,14 @@ export interface PositionId extends u64 {}
 
 /** @name PreRuntime */
 export interface PreRuntime extends ITuple<[ConsensusEngineId, Bytes]> {}
+
+/** @name ProxyType */
+export interface ProxyType extends Enum {
+  readonly isAny: boolean;
+  readonly isNonTransfer: boolean;
+  readonly isGovernance: boolean;
+  readonly isStaking: boolean;
+}
 
 /** @name Rate */
 export interface Rate extends Fixed128 {}
