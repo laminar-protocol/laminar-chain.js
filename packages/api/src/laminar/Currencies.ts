c052f930cae9d8ad427d4d8e3be62a3c98df8b5d
aniiantt <nsso@foxmail.com>
2020-07-07 12:31:56 +1200

    import order

--- packages/api/src/laminar/Currencies.ts
+++ packages/api/src/laminar/Currencies.ts
@@ -1,8 +1,8 @@
+import BN from 'bn.js';
 import { combineLatest, Observable, of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
-import { TokenId, OracleValue, TokenBalance, TokenInfo } from '../types';
+import { OracleValue, TokenBalance, TokenId, TokenInfo } from '../types';
 import LaminarApi from './LaminarApi';
-import BN from 'bn.js';
 
 class Currencies {
   private apiProvider: LaminarApi;
