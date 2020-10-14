85842ef08943ad67779520211d83a68d87f8bcf5
aniiantt <nxiiun@gmail.com>
2020-10-15 11:28:00 +1300

    fix jest

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,10 @@ module.exports = {
     '@laminar/types(.*)$': '<rootDir>/packages/types/src/$1',
     '@laminar/type-definitions(.*)$': '<rootDir>/packages/type-definitions/src/$1'
   },
-  modulePathIgnorePatterns: ['<rootDir>/build', '<rootDir>/packages/api/build', '<rootDir>/packages/types/build']
+  modulePathIgnorePatterns: [
+    '<rootDir>/build',
+    '<rootDir>/packages/api/build',
+    '<rootDir>/packages/types/build',
+    '<rootDir>/packages/type-definitions/build'
+  ]
 };
